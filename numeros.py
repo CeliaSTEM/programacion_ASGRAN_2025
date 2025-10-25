e1dc946fe54bc225294c29e3dc744c0842adf73c
CeliaSTEM <145722929+CeliaSTEM@users.noreply.github.com>
2025-10-25 13:09:32 +0200

    Operadores básicos

--- numeros.py
+++ numeros.py
@@ -1,5 +1,71 @@
+import random
+
 numero_str = "2"        # String -> Cadena de caracteres.
 
+numero_int_uno = 3          # Integer -> Número entero.
+numero_int_dos = 5
+numero_float_uno = 3.5      # Float -> Número en coma flotante.
+numero_float_dos = 5.5
 
-numero_int = 3          # Integer -> Número entero.
-numero_float = 3.0      # Float -> Número en coma flotante.
+numero_aleatorio = random.randrange(1,6)    # El 6 no está incluido
+print(numero_aleatorio)
+
+# Suma
+print("Numero entero + Numero entero: ", end="")
+print(numero_int_uno+numero_int_dos)
+print("Numero entero + Numero decimal: ", end="")
+print(numero_int_uno+numero_float_uno)
+print("Numero decimal + Numero decimal: ", end="")
+print(numero_float_uno+numero_float_dos)
+
+# Resta
+print("Numero entero - Numero entero: ", end="")
+print(numero_int_uno-numero_int_dos)
+print("Numero entero - Numero decimal: ", end="")
+print(numero_int_uno-numero_float_uno)
+print("Numero decimal - Numero decimal: ", end="")
+print(numero_float_uno-numero_float_dos)
+
+# Multiplicación
+print("Numero entero x Numero entero: ", end="")
+print(numero_int_uno*numero_int_dos)
+print("Numero entero x Numero decimal: ", end="")
+print(numero_int_uno*numero_float_uno)
+print("Numero decimal x Numero decimal: ", end="")
+print(numero_float_uno*numero_float_dos)
+
+# División
+print("Numero entero / Numero entero: ", end="")
+print(numero_int_uno/numero_int_dos)
+print("Numero entero / Numero decimal: ", end="")
+print(numero_int_uno/numero_float_uno)
+print("Numero decimal / Numero decimal: ", end="")
+print(numero_float_uno/numero_float_dos)
+
+# División exacta
+print("Numero entero // Numero entero: ", end="")
+print(numero_int_uno//numero_int_dos)
+print("Numero entero // Numero decimal: ", end="")
+print(numero_int_uno//numero_float_uno)
+print("Numero decimal // Numero decimal: ", end="")
+print(numero_float_uno//numero_float_dos)
+
+print("Ejemplo división exacta: \n    Si tengo 20 caramelos y son 8 niños, ¿cuántos caramelos se come cada uno?")
+print(f"    Tocamos a {20//8} caramelos por niño.")
+
+# Exponente
+
+print("Numero entero ** Numero entero: ", end="")
+print(numero_int_uno**numero_int_dos)
+print("Numero entero ** Numero decimal: ", end="")
+print(numero_int_uno**numero_float_uno)
+print("Numero decimal ** Numero decimal: ", end="")
+print(numero_float_uno**numero_float_dos)
+
+# Módulo de una división
+print("\nEjemplo división exacta y módulo: ")
+print( "    Si tengo 20 caramelos y son 8 niños... ")
+print( "    ¿Cuántos caramelos se come cada uno?")
+print(f"    Tocamos a {20//8} caramelos por niño.")
+print( "    ¿Cuántos caramelos sobran?")
+print(f"    Sobran {20%8} caramelos.")
