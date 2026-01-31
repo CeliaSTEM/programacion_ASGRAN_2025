56b3c0fbe10f3cd656b8878f79f21a6dd98dc2b3
CeliaSTEM <145722929+CeliaSTEM@users.noreply.github.com>
2026-01-31 12:59:21 +0100

    PiedraPapelTijera lógica completa
    Refactor game loop to allow player to re-enter choice and track scores.

--- PPT.py
+++ PPT.py
@@ -2,27 +2,43 @@ import getpass
 import random
 
 lista_opciones = ["piedra", "papel", "tijera"]
+
+puntos_jugador_1 = 0 
+puntos_ia = 0
+
 # eleccion_jugador_1 = input("JUGADOR 1: Piedra, papel, tijera -> ").lower()
 # eleccion_jugador_2 = input ("JUGADOR 2: Piedra, papel, tijera -> ").lower()
-eleccion_jugador_1 = getpass.getpass(("JUGADOR 1: Piedra, papel, tijera -> ").lower())
-while eleccion_jugador_1 not in lista_opciones:
-    print(f"Has escrito {eleccion_jugador_1}")
-    eleccion_jugador_1 = getpass.getpass(("JUGADOR 1 (otra vez): Piedra, papel, tijera -> ").lower())
+
+al_mejor_de_3 = 2
+
+salir = "NO"
+while salir != "SI" and puntos_jugador_1 < al_mejor_de_3 and puntos_ia < al_mejor_de_3:
+
+    eleccion_jugador_1 = getpass.getpass(("JUGADOR 1: Piedra, papel, tijera -> ")).lower()
+    while eleccion_jugador_1 not in lista_opciones:
+        print(f"Has escrito {eleccion_jugador_1}")
+        eleccion_jugador_1 = getpass.getpass(("JUGADOR 1 (otra vez): Piedra, papel, tijera -> ")).lower()
 
 
-#eleccion_jugador_2 = getpass.getpass(("JUGADOR 2: Piedra, papel, tijera -> ").lower())
-eleccion_pc = random.choice(lista_opciones)
-print(f"Elección jugador 1: {eleccion_jugador_1}")
-#print(f"Elección jugador 2: {eleccion_jugador_2}")
-print(f"Elección pc: {eleccion_pc}")
+    #eleccion_jugador_2 = getpass.getpass(("JUGADOR 2: Piedra, papel, tijera -> ").lower())
+    eleccion_pc = random.choice(lista_opciones)
+    print(f"Elección jugador 1: {eleccion_jugador_1}")
+    #print(f"Elección jugador 2: {eleccion_jugador_2}")
+    print(f"Elección pc: {eleccion_pc}")
 
-# Cuarta forma: ¿Funciona? Lo mejor que podemos hacer con if/else [sin vectores circulares]
-if eleccion_jugador_1 == eleccion_pc:
-    print ("Empate")
-elif (eleccion_jugador_1 == "piedra" and eleccion_pc == "tijera") or (eleccion_jugador_1 == "papel" and eleccion_pc == "piedra") or (eleccion_jugador_1 == "tijera" and eleccion_pc == "papel"):
-    print ("Gana el jugador 1")
-else:
-    print ("Gana la IA")
+    # Cuarta forma: ¿Funciona? Lo mejor que podemos hacer con if/else [sin vectores circulares]
+    if eleccion_jugador_1 == eleccion_pc:
+        print ("Empate")
+    elif (eleccion_jugador_1 == "piedra" and eleccion_pc == "tijera") or (eleccion_jugador_1 == "papel" and eleccion_pc == "piedra") or (eleccion_jugador_1 == "tijera" and eleccion_pc == "papel"):
+        print ("Gana el jugador 1")
+        puntos_jugador_1 += 1
+    else:
+        print ("Gana la IA")
+        puntos_ia += 1
+
+    salir = input("¿Quieres salir del juego? SI/NO: ").upper()
+
+print(f"El jugador 1 ha ganadado {puntos_jugador_1} veces y la IA ha ganado {puntos_ia} veces.")
 
 # TODO:
 # - Vamos a gestionar turnos. Para que si el humano se equivaca metiendo su elección, le permita repetir.
